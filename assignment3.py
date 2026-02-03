ecdb04bedd94e5055dbebadc6986553ff8b7f485
okerekeo-a11y <okerekeo@students.trinitydc.edu>
2026-02-03 15:23:18 -0500

    commit for week2 a3

---
+++ assignment3.py
@@ -0,0 +1,14 @@
+"""
+Ogechukwu Okereke
+CSMC 111
+Spring 2026
+week2 assignment3
+"""
+age = int(input("Enter your age: "))
+
+if age > 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
+print(age)
+
