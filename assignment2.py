2b1ab0bef92a95264d40d8505a8973a06faa611d
okerekeo-a11y <okerekeo@students.trinitydc.edu>
2026-02-03 15:32:34 -0500

    commit to week2 a2

---
+++ assignment2.py
@@ -0,0 +1,12 @@
+"""
+Ogechukwu Okereke
+CSMC 111
+Spring 2026
+week2 assignment2
+"""
+status = bool(input("Are you an adult "))
+
+if status is True:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
